2a93b0259785bc42939daafbab48dba18bb26700
Prosper <onungwap@gmail.com>
2018-08-14 11:33:53 -0400

    Implemented Preferences

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -20,11 +20,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -38,7 +42,7 @@ import java.util.List;
 public class EarthquakeActivity extends AppCompatActivity implements LoaderCallbacks<List<Earthquake>> {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
-    private static final String USGS_REQUES_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=5&limit=10";
+    private static final String USGS_REQUES_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query";
     private EarthquakeAdapter mAdapter;
     private static final int EARTHQUAKE_LOADER_ID = 0;
     // Find a reference to the {@link ListView} in the layout
@@ -73,8 +77,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
 
         if (isConnected) {
             getLoaderManager().initLoader(EARTHQUAKE_LOADER_ID, null, this);
-        }
-        else {
+        } else {
             mProgressBar.setVisibility(View.GONE);
             TextView noNetwork = (TextView) findViewById(R.id.no_network);
             noNetwork.setText(R.string.no_internet);
@@ -98,7 +101,25 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
 
     @Override
     public Loader<List<Earthquake>> onCreateLoader(int id, Bundle args) {
-        return new EarthquakeLoader(this, USGS_REQUES_URL);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String minMagnitude = sharedPreferences.getString(getString(R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default));
+        String limit = sharedPreferences.getString(getString(R.string.settings_limit_key),
+                getString(R.string.settings_limit_default));
+        String maxMagnitude = sharedPreferences.getString(getString(R.string.settings_max_magnitude_key),
+                getString(R.string.settings_max_magnitude_default));
+        String orderby = sharedPreferences.getString(getString(R.string.settings_orderby_key),
+                getString(R.string.settings_orderby_default));
+
+        Uri baseUri = Uri.parse(USGS_REQUES_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("limit", limit);
+        uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("maxmagnitude", maxMagnitude);
+        uriBuilder.appendQueryParameter("orderby", orderby);
+
+        return new EarthquakeLoader(this, uriBuilder.toString());
     }
 
     @Override
@@ -117,6 +138,23 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
     @Override
     public void onLoaderReset(Loader<List<Earthquake>> loader) {
         mAdapter.clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
